eb4831e20825c3f7e25b9fa06857a73531cedc3e
Jaysmart <ogunbunmijoshua60@gmail.com>
2025-08-24 00:30:53 +0100

    add new images for SectionTwo; update SectionTwo.jsx for layout adjustments

--- src/components/secondaryComponents/SectionTwo/SectionTwo.jsx
+++ src/components/secondaryComponents/SectionTwo/SectionTwo.jsx
@@ -12,7 +12,11 @@ const SectionTwo = () => {
           <div id='section-one-circle' className='section-one-circle'>
           <Circle image={Excited}  anchortext={'Click me'}/>
           </div>
-       
+
+          <div>
+            
+          </div>
+
         </div>
     </>
   )
